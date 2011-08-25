df1cfa67f5e3960ad0269ae2cf30570cb197e679
Julien Ponge <julien.ponge@gmail.com>
2011-08-25 10:16:48 +0200

    Refreshed the stats viewer UI.
    * jQuery effects
    * Twitter Bootstrap CSS

--- static/stats-viewer/viewer.js
+++ static/stats-viewer/viewer.js
@@ -18,31 +18,33 @@
 
 function go() {
     
+    installGUIHandlers();
+    
     getData(function(stats) {
         $.each(stats, function(product, productStats) {
-            $('#global-stats').append(
+            $('#global-stats > tbody').append(
                 $('<tr><td>' + product + '</td><td>' + productStats.all + '</td></tr>')
             );
             $.each(productStats, function(version, versionStats) {
                 if (version != 'all') {
-                    $('#version-stats').append(
+                    $('#version-stats > tbody').append(
                         $('<tr><td>' + product + '</td><td>' + 
                           version + '</td><td>' + versionStats.all + '</td></tr>')
                     );
                     $.each(versionStats.yearly, function(year, value) {
-                        $('#yearly-stats').append(
+                        $('#yearly-stats > tbody').append(
                             $('<tr><td>' + year + '</td><td>' + product + '</td><td>' +
                               version + '</td><td>' + value + '</td></tr>')
                         );
                     });                    
                     $.each(versionStats.monthly, function(month, value) {
-                        $('#monthly-stats').append(
+                        $('#monthly-stats > tbody').append(
                             $('<tr><td>' + month + '</td><td>' + product + '</td><td>' +
                               version + '</td><td>' + value + '</td></tr>')
                         );
                     });
                     $.each(versionStats.daily, function(day, value) {
-                        $('#daily-stats').append(
+                        $('#daily-stats > tbody').append(
                             $('<tr><td>' + day + '</td><td>' + product + '</td><td>' +
                               version + '</td><td>' + value + '</td></tr>')
                         );
@@ -53,12 +55,38 @@ function go() {
     });
     
     getMap(function(map, points) {
+        $('#spinner > h3 > span').text('Loaded!');
+        $('#spinner').fadeOut(3000);
         var image = '/stats-viewer/spot-32.png';
         points.forEach(function(point) {
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(point.latitude, point.longitude), 
                 map: map, 
                 icon: image
+            });
+        });
+    });
+}
+
+// .............................................................................................. //
+
+function installGUIHandlers() {
+    var views = [
+        '#map', '#global-stats', '#version-stats', '#yearly-stats', '#monthly-stats', '#daily-stats'
+    ];
+    var buttons = [
+        '#map-button', '#global-button', '#version-button', '#yearly-button', '#monthly-button', '#daily-button'
+    ];
+    var activeMainView = '#map';
+    var activeButton = '#map-button';
+    buttons.forEach(function(button, index) {
+        $(button + ' > a').click(function() {
+            $(activeButton).removeClass('active');
+            $(button).addClass('active');
+            $(activeMainView).fadeOut('fast', function() {
+                $(views[index]).fadeIn('fast');
+                activeButton = button;
+                activeMainView = views[index];
             });
         });
     });
